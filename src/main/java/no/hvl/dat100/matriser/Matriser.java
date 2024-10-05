95914f176ace96e137f16e90fc981170696f5af0
h673018 <142883570+h673018@users.noreply.github.com>
2024-10-05 05:38:23 +0200

    Update Matriser.java

--- src/main/java/no/hvl/dat100/matriser/Matriser.java
+++ src/main/java/no/hvl/dat100/matriser/Matriser.java
@@ -4,32 +4,58 @@ public class Matriser {
 
 	// a)
 	public static void skrivUt(int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("Metoden skrivUt ikke implementert");
+	    for (int[] rad : matrise) {
+	        for (int element : rad) {
+	            System.out.print(element + " "); 
+	        }
+	        System.out.println(); 
+	    }
 	}
 
 	// b)
 	public static String tilStreng(int[][] matrise) {
+	    StringBuilder sb = new StringBuilder();
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
+	    for (int[] rad : matrise) {
+	        for (int element : rad) {
+	            sb.append(element).append(" "); 
+	        }
+	        sb.append("\n"); 
+	    }
+
+	    return sb.toString();
 		
 	}
 
 	// c)
 	public static int[][] skaler(int tall, int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("Metoden skaler ikke implementert");
+	    int[][] nyMatrise = new int[matrise.length][matrise[0].length]; 
+
+	    for (int i = 0; i < matrise.length; i++) {
+	        for (int j = 0; j < matrise[i].length; j++) {
+	            nyMatrise[i][j] = matrise[i][j] * tall; 
+	        }
+	    }
+
+	    return nyMatrise; 
 	
 	}
 
 	// d)
-	public static boolean erLik(int[][] a, int[][] b) {
+	public static boolean erLik(int[][] mat1, int[][] mat2) {
+	    if (mat1.length != mat2.length || mat1[0].length != mat2[0].length) {
+	        return false; 
+	    }
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden erLik ikke implementert");
+	    for (int i = 0; i < mat1.length; i++) {
+	        for (int j = 0; j < mat1[i].length; j++) {
+	            if (mat1[i][j] != mat2[i][j]) {
+	                return false; 
+	            }
+	        }
+	    }
+
+	    return true; 
 		
 	}
 	
